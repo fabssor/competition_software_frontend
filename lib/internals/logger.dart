4f804ca1a42ac5a60f0ffbc3ea1a0c7605a7fcff
fabssor <30718590+fabssor@users.noreply.github.com>
2021-12-28 21:19:31 +0100

    removes dead code

--- lib/internals/logger.dart
+++ lib/internals/logger.dart
@@ -9,25 +9,13 @@ class Logger {
   }
 
   static void logSnackbar(BuildContext context, String logString) {
-    // SnackBar snackBar = SnackBar(
-    //   content: Text(
-    //     logString,
-    //     textAlign: TextAlign.center,
-    //   ),
-    //   width: null,
-    //   behavior: SnackBarBehavior.floating,
-    //   shape: const RoundedRectangleBorder(
-    //     borderRadius: BorderRadius.all(Radius.circular(10.0)),
-    //   ),
-    // );
-    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
     Flushbar(
       messageText: Text(
         logString,
         textAlign: TextAlign.center,
       ),
-      duration: Duration(seconds: 3),
-      margin: EdgeInsets.all(8),
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(8),
       borderRadius: BorderRadius.circular(8),
       flushbarStyle: FlushbarStyle.FLOATING,
       icon: const Icon(Icons.info_outline),
