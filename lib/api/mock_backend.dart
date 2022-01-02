0b8b760a0af0997768c9e51ca15d73d2caca23f3
fabssor <30718590+fabssor@users.noreply.github.com>
2022-01-02 12:45:25 +0100

    Adds some dummy persons

--- lib/api/mock_backend.dart
+++ lib/api/mock_backend.dart
@@ -3,8 +3,19 @@ import 'package:competition_software_frontend/api/competitor.dart';
 import 'package:competition_software_frontend/api/i_backend.dart';
 import 'package:competition_software_frontend/internals/logger.dart';
 
-List<Competitor> competitors = <Competitor>[];
-int nextId = 0;
+List<Competitor> competitors = <Competitor>[
+  Competitor.withId(0, "Max", "Mustermann", Gender.male, DateTime(1992, 6, 23)),
+  Competitor.withId(1, "Emily", "Müller", Gender.female, DateTime(1990, 3, 12)),
+  Competitor.withId(2, "Martin", "Bauer", Gender.male, DateTime(2010, 12, 12)),
+  Competitor.withId(
+      3, "Martina", "Bauer", Gender.female, DateTime(2005, 12, 24)),
+  Competitor.withId(4, "Markus", "Roth", Gender.male, DateTime(2000, 5, 23)),
+  Competitor.withId(5, "Jan-Torsten", "Rum", Gender.male, DateTime(1999, 6, 8)),
+  Competitor.withId(6, "Star", "Maier", Gender.female, DateTime(1999, 6, 29)),
+  Competitor.withId(7, "Emil", "Bäcker", Gender.male, DateTime(2009, 1, 1)),
+  Competitor.withId(9, "Karmen", "Bäcker", Gender.female, DateTime(2010, 1, 3)),
+];
+int nextId = competitors.length;
 
 class MockBackend implements IBackend {
   @override
